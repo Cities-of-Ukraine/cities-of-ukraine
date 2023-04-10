a4f45766dd620294870c61280819d7505cf1ad88
Ibrahim Saberi <georgeipsum@gmail.com>
2023-04-10 17:58:16 -0400

    some more resources

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -5,14 +5,10 @@ import { useEffect } from "react";
 const Map: React.FC = () => {
   useEffect(() => {
     import("leaflet").then(L => {
-      const map = L.map("map").setView([51.505, -0.09], 13);
-      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          maxZoom: 19,
-          attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-      }).addTo(map);
+      const map = L.map("map").setView([51.505, -0.09], 6);
     });
   }, []);
-  return <div style={{ height: "100%", width: "100%" }}><div id="map"></div></div> 
+  return <div style={{ height: "100%", width: "100%" }}><div id="map"></div></div>;
 };
 
 export default Map;
