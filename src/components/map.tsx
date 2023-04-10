3e768dc77c1720165a347749cb3ab0c9b035dd6a
Ibrahim Saberi <georgeipsum@gmail.com>
2023-04-10 17:59:29 -0400

    asdf

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -6,9 +6,14 @@ const Map: React.FC = () => {
   useEffect(() => {
     import("leaflet").then(L => {
       const map = L.map("map").setView([51.505, -0.09], 6);
+
+      L.tileLayer('https://tile.openstreetmap.org/6/51/9.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      }).addTo(map);
     });
   }, []);
-  return <div style={{ height: "100%", width: "100%" }}><div id="map"></div></div>;
+  return <div style={{ height: 180, width: 180 }}><div id="map" /></div>;
 };
 
 export default Map;
